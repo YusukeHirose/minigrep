a1dc1ff4bed93aba95aa9271f08a0d9aa2b1d204
YusukeHirose <kodecurry@gmail.com>
2020-04-24 00:19:48 +0900

    feat: ファイルを読み込む

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -7,4 +10,14 @@ fn main() {
     let file_name = &args[2];
     println!("Searching for {}", query);
     println!("In file {}", file_name);
+
+    println!("In file {}", file_name);
+
+    let mut f = File::open(file_name).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
