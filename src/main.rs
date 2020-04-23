e34933f587595de9758b3ffc4a1c1e5f67f67a6a
YusukeHirose <kodecurry@gmail.com>
2020-04-24 00:37:07 +0900

    fix: タプルから構造体に変更

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_name) = parse_config(&args);
+    let config = parse_config(&args);
 
-    let mut f = File::open(file_name).expect("file not found");
+    let mut f = File::open(config.file_name).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -15,9 +15,14 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
     // プログラム名がVecの[0]だから1から指定
-    let query = &args[1];
-    let file_name = &args[2];
-    (query, file_name)
+    let query = &args[1].clone();
+    let file_name = &args[2].clone();
+    Config { query, file_name }
 }
